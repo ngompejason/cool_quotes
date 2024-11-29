50134fa2da3392c1923b4e710cbc3ae43d5cf5fa
HelpDesk <jasonngompe49@gmail.com>
2024-11-29 13:59:23 +0100

    style the post quote page

--- quotes/forms.py
+++ quotes/forms.py
@@ -5,12 +5,17 @@ class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ["verse","holy_book","verse_ref"]
+        
+        widgets = {field: forms.TextInput(attrs={'class': 'form-input'})
+                   for field in fields}
     
     def clean_holy_book(self):
         holy_book = self.cleaned_data["holy_book"]
         capitalize_title = ' '.join([word.capitalize() for word in holy_book.split()])
         
         return capitalize_title
+    
+            
     
 
 class ReportForm(forms.ModelForm):
--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.getElementById('profileMenuToggle').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
@@ -12,4 +14,28 @@ window.addEventListener('click', function(event) {
         menu.style.display = 'none';
     }
 });
-  
+
+
+//==========================================================
+//----------------------Create Quote Script------------------------
+//==========================================================
+
+// Add event listener for focus
+document.querySelectorAll('input').forEach(function(input) {
+    input.addEventListener('focus', function() {
+        this.closest('.form-group').classList.add('focused');
+    });
+});
+
+// Add event listener for blur
+document.querySelectorAll('input').forEach(function(input) {
+    input.addEventListener('blur', function() {
+        var inputValue = this.value;
+        if (inputValue === "") {
+            this.classList.remove('filled');
+            this.closest('.form-group').classList.remove('focused');
+        } else {
+            this.classList.add('filled');
+        }
+    });
+});
